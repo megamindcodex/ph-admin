058b0615bd8665f14f5caa1f22bd2a631f40ddc3
Emmancodex <megamindcodex@gmail.com>
2024-03-09 23:51:49 +0100

    modified the enpoint url

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,3 +1,3 @@
 //cyclic url endpoint
-// export const endpoint = "https://cute-lime-cocoon-hat.cyclic.app";
-export const endpoint = "http://localhost:5000";
+export const endpoint = "https://weak-rose-goldfish-wig.cyclic.app";
+// export const endpoint = "http://localhost:5000";
